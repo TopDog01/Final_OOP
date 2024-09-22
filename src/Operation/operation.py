f76eec0b953478b70a6af8731bebc7284cbee675
Yuri Konev <crazy_clown93@mail.ru>
2024-09-22 13:36:19 +0300

    operation

---
+++ src/Operation/operation.py
@@ -0,0 +1,11 @@
+class AddOperation:
+    def execute(self, num1, num2):
+        return num1 + num2
+
+class MultiplyOperation:
+    def execute(self, num1, num2):
+        return num1 * num2
+
+class DivideOperation:
+    def execute(self, num1, num2):
+        return num1 / num2
